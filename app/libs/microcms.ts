3dec479e188020736d85fab277b788cf61b843a3
kmknka <kaichi0mellow@gmail.com>
2025-07-28 11:08:01 +0900

    環境変数修正

--- app/libs/microcms.ts
+++ app/libs/microcms.ts
@@ -6,8 +6,8 @@ type tagList = {
 };
 
 export const client = createClient({
-  serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN || "", // service-domain は https://XXXX.microcms.io の XXXX 部分
-  apiKey: import.meta.env.MICROCMS_API_KEY || "",
+  serviceDomain: import.meta.env.VITE_MICROCMS_SERVICE_DOMAIN || "", // service-domain は https://XXXX.microcms.io の XXXX 部分
+  apiKey: import.meta.env.VITE_MICROCMS_API_KEY || "",
 });
 
 export const getBlogs = async (queries: MicroCMSQueries) => {
