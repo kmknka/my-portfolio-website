d28af077186b84098ef7ba81e3c1436ea5235562
kmknka <kaichi0mellow@gmail.com>
2025-07-31 16:10:21 +0900

    ルート背景色設定

--- app/root.tsx
+++ app/root.tsx
@@ -21,7 +21,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body className="min-h-screen">
+      <body className="min-h-screen bg-white">
         {/* ヘッダー */}
         <Header />
         {/* モバイル表示時のみ余白を追加 */}
